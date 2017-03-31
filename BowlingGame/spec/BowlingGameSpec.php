5991973b80321127351ac587cd1128667bfc3425
Roman Ganoshenko <roman.coding@gmail.com>
2017-03-31 22:08:36 +0300

    Complete Bowling Game challenge

--- BowlingGame/spec/BowlingGameSpec.php
+++ BowlingGame/spec/BowlingGameSpec.php
@@ -8,8 +8,46 @@ use Prophecy\Argument;
 
 class BowlingGameSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_returns_score_after_spare()
     {
-        $this->shouldHaveType(BowlingGame::class);
+    	$this->hit(5, 2);
+    	$this->hit(6, 4);
+    	$this->hit(4, 2);
+    	$this->getScore()->shouldReturn(27);
     }
+
+    public function it_conts_score_after_strike()
+    {
+    	$this->hit(10);
+    	$this->hit(2, 4);
+    	$this->getScore()->shouldReturn(22);
+    }
+
+    public function it_counts_perfect_game()
+    {
+    	for ($i = 0; $i < 12; $i++)
+    	{
+    		$this->hit(10);
+    	}
+    	$this->getScore()->shouldReturn(300);
+    }
+
+    public function it_counts_score_for_full_game()
+    {
+    	$this->hit(10); // strike
+    	$this->hit(3, 7); // spare
+    	$this->hit(6, 1);
+    	$this->hit(10); // strike
+    	$this->hit(10); // strike
+    	$this->hit(10); // strike
+    	$this->hit(2, 8); // spare
+    	$this->hit(9, 0);
+    	$this->hit(7, 3); // spare
+    	$this->hit(10); // strike
+    	/* Bonus hits for last strike */
+    	$this->hit(10); // strike
+    	$this->hit(10); // strike
+    	$this->getScore()->shouldReturn(193);
+    }
+
 }
--- BowlingGame/src/BowlingGame.php
+++ BowlingGame/src/BowlingGame.php
@@ -2,4 +2,107 @@
 
 class BowlingGame
 {
+
+	/**
+	 * All registered rolls for a game.
+	 *
+	 * @var array
+	 */
+	protected $rolls = [];
+
+
+	/**
+	 * Hit for a frame.
+	 *
+	 * @param integer $first 
+	 * @param integer $second
+	 * @return void
+	 */
+	public function hit(int $first, int $second = 0)
+	{
+		$this->rolls[] = [$first, $second];
+	}
+
+	/**
+	 * Calculate the final score for a game.
+	 *
+	 * @return int
+	 */
+	public function getScore(): int
+	{
+		$score = 0;
+		foreach ($this->rolls as $frame => $roll) 
+		{
+			$score += $this->getFrameScore($roll);
+			if ($this->isStrike($roll) && $frame < 9)
+			{
+				$score += $this->getStrikeBonus($frame);
+				if ($this->isStrike($this->rolls[$frame+1]))
+				{
+					$score += $this->getSpareBonus($frame+1);
+				}
+				continue;
+			}
+			if ($this->isSpare($roll) && $frame < 9)
+			{
+				$score += $this->getSpareBonus($frame);
+			}
+		}
+		return $score;
+	}
+
+	/**
+	 * Did the player make a spare?
+	 *
+	 * @param $roll
+	 * @return bool
+	 */
+	private function isSpare(array $roll): bool
+	{
+		return ($roll[0] + $roll[1] == 10);
+	}
+
+	/**
+	 * Did the player make a strike?
+     *
+	 * @param $roll
+	 * @return bool
+	 */
+	private function isStrike(array $roll): bool
+	{
+		return ($roll[0] == 10);
+	}
+
+	/**
+	 * Get the default sum of the two rolls for a frame.
+	 *
+	 * @param $roll
+	 * @return int
+	 */
+	private function getFrameScore(array $roll): int
+	{
+		return array_sum($roll);
+	}
+
+	/**
+	 * Get the bonus for a spare in a frame.
+	 *
+	 * @param $roll
+	 * @return mixed
+	 */
+	private function getSpareBonus(int $frame): int
+	{
+		return $this->rolls[$frame+1][0] ?? 0;
+	}
+
+	/**
+	 * Get the bonus for a strike in a frame.
+	 *
+	 * @param $roll
+	 * @return mixed
+	 */
+	private function getStrikeBonus(int $frame): int
+	{
+		return array_sum($this->rolls[$frame+1] ?? []);
+	}
 }
